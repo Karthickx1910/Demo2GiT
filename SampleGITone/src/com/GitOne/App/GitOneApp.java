13df732eaaf509630460c97640ffb04c3da4f86e
karthickx1910 <karthickx1910@gmail.com>
2020-02-26 08:06:49 +0530

    first commit

---
+++ SampleGITone/src/com/GitOne/App/GitOneApp.java
@@ -0,0 +1,11 @@
+package com.GitOne.App;
+
+public class GitOneApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("ggfdhdhh ");
+	}
+
+}
